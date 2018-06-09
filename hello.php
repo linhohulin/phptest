30356186a040d66fd1c7e89e295b4f3aa5b9547d
dh <lin.hohulin@tx.rr.com>
2018-06-09 08:41:16 -0500

    first php file

---
+++ hello.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+      <title>Hello World</title>
+    </head>
+    <body>
+        <?php
+           print "Hello world<br />";
+           print "Goodbye.<br />";
+           print "Yada.";
+        ?>
+    </body>
+</html>
